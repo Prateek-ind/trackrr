c6280cd653ea6b0d7fb9d7b14aaebe865a1fbe24
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-05-14 11:31:20 +0530

    Add authentication middleware to job routes for access control

---
+++ server/middlewares/protect.middleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
+
+const protect = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized access",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Invalid token",
+    });
+  }
+};
+
+module.exports = protect;
--- server/routes/job.routes.js
+++ server/routes/job.routes.js
@@ -1,11 +1,18 @@
-const express = require("express")
-const router = express.Router()
-const { getJobs, getJobById, createJob, updateJob, deleteJob } = require("../controllers/job.controller")
+const express = require("express");
+const router = express.Router();
+const {
+  getJobs,
+  getJobById,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/job.controller");
+const protect = require("../middlewares/protect.middleware");
 
-router.get("/", getJobs)
-router.get("/:id", getJobById)
-router.post("/", createJob)
-router.post("/:id", updateJob)
-router.post("/:id", deleteJob)
+router.get("/", protect, getJobs);
+router.get("/:id", protect, getJobById);
+router.post("/", protect, createJob);
+router.put("/:id", protect, updateJob);
+router.delete("/:id", protect, deleteJob);
 
-module.exports = router
+module.exports = router;
