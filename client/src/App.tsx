958dc15a266148d01466f7e6757b5e9cfe885e49
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-06-14 11:16:20 +0530

    fix: adjust formatting in App component for improved readability

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,11 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router/AppRouter";
 import { ThemeProvider } from "@/components/Theme-Provider";
 
+
 const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="trackrr-theme">
+     
       <RouterProvider router={router} />
     </ThemeProvider>
   );
