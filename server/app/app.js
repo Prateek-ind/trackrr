486518953385a08aa32342dbba49cd15a3541ae9
Prateek Kumar <prateek.kumar.ind@gmail.com>
2026-06-14 13:01:09 +0530

    fix: update CORS configuration to allow multiple origins for improved flexibility

--- server/app/app.js
+++ server/app/app.js
@@ -15,9 +15,20 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "https://trackrr-red.vercel.app",
+  "http://localhost:5173",
+];
+
 app.use(
   cors({
-    origin: "https://trackrr-red.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
